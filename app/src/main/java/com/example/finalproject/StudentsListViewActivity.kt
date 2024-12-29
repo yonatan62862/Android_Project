9b9c3d95039a86156635d70e3f0131a36103c3e1
Yonatan <yonatan62862@gmail.com>
2024-12-29 16:03:05 +0200

    Create Adapter and ViewHolder

---
+++ app/src/main/java/com/example/finalproject/StudentsListViewActivity.kt
@@ -0,0 +1,113 @@
+package com.example.finalproject
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.CheckBox
+import android.widget.ListView
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.finalproject.model.Model
+import com.example.finalproject.model.Student
+
+class StudentsListViewActivity : AppCompatActivity() {
+
+    var students: MutableList<Student>? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_students_list_view)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // 1. Set xml layout✅
+        // 2. Set instance of list view in activity✅
+        // 3. Set adapter✅
+        // 4. Create rows layout✅
+        // 5. Set dynamic data (MVP)✅
+        // 6. On click on checkbox ✅
+
+        students = Model.shared.students
+        val listView: ListView = findViewById(R.id.students_list_view)
+        listView.adapter = StudentsAdapter()
+    }
+
+    inner class StudentsAdapter(): BaseAdapter() {
+        override fun getCount(): Int = students?.size ?: 0
+
+        override fun getItem(position: Int): Any {
+            TODO("Not yet implemented")
+        }
+
+        override fun getItemId(position: Int): Long {
+            TODO("Not yet implemented")
+        }
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+            val inflation= LayoutInflater.from(parent?.context)
+            val view = convertView ?: inflation.inflate(
+                R.layout.student_list_row,
+                parent,
+                false
+            ).apply {
+                findViewById<CheckBox>(R.id.student_row_check_box).apply {
+                    setOnClickListener { view ->
+                        (tag as? Int)?.let { tag ->
+                            val student = students?.get(tag)
+                            student?.isChecked = (view as? CheckBox)?.isChecked ?: false
+                        }
+
+                    }
+                }
+            }
+
+//            var view = convertView
+//            if (view == null) {
+//                view = inflation.inflate(R.layout.student_list_row, parent, false)
+//                Log.d("TAG", "Inflating position $position")
+//                val checkBox: CheckBox? = view?.findViewById(R.id.student_row_check_box)
+////                checkBox?.setOnClickListener {
+////                    student?.isChecked = checkBox.isChecked
+////                }
+//
+//                checkBox?.apply {
+//                    setOnClickListener { view ->
+//                        (tag as? Int)?.let { tag ->
+//                            val student = students?.get(tag)
+//                            student?.isChecked = (view as? CheckBox)?.isChecked ?: false
+//                        }
+//
+//                    }
+//                }
+//            }
+            val student = students?.get(position)
+
+            val nameTextView: TextView? = view?.findViewById(R.id.student_row_name_text_view)
+            val idTextView: TextView? = view?.findViewById(R.id.student_row_id_text_view)
+            val checkBox: CheckBox? = view?.findViewById(R.id.student_row_check_box)
+
+
+            nameTextView?.text = student?.name
+            idTextView?.text = student?.id
+//            checkBox?.isChecked = student?.isChecked ?: false
+
+            checkBox?.apply {
+                isChecked = student?.isChecked ?: false
+                tag = position
+            }
+
+            return view!!
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/finalproject/model/Student.kt
@@ -0,0 +1,8 @@
+package com.example.finalproject.model
+
+class Student (
+    val name: String,
+    val id: String,
+    val avatarUrl: String,
+    var isChecked: Boolean
+)
